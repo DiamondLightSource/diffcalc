80a20225c3acdd628bcdb8b870cdf5706ecf263e
Rob Walton <rob.walton@diamond.ac.uk>
2013-08-12 21:20:33 +0100

    Flatten conf.settings down to settings

--- diffcalc/settings.py
+++ diffcalc/settings.py
@@ -3,61 +3,60 @@ Created on Aug 5, 2013
 
 @author: walton
 '''
-from diffcalc.hkl.vlieg.calc import VliegUbCalcStrategy, vliegAnglesToHkl
-from diffcalc.hkl.willmott.calc import WillmottHorizontalUbCalcStrategy,\
-    angles_to_hkl
-from diffcalc.hkl.you.calc import YouUbCalcStrategy, youAnglesToHkl
+import os 
+
+import diffcalc.hkl.vlieg.calc
+import diffcalc.hkl.willmott.calc
+import diffcalc.hkl.you.calc
 from diffcalc.ub.persistence import UBCalculationJSONPersister
 
+###############################################################################
+# These should be set via configure()  
+geometry = None
+hardware = None
+ubcalc_persister = UBCalculationJSONPersister(os.getcwd())
 
-AVAILABLE_ENGINES = ('vlieg', 'willmott', 'you')
+#  Used only by diffcalc.gdasupport
+axes_scannable_group = None
+energy_scannable = None
+energy_scannable_multiplier_to_get_KeV=1
 
+# These will be set by dcyou, dcvlieg or dcwillmot
+ubcalc_strategy = None
+angles_to_hkl_function = None
+###############################################################################
+   
+def configure(hardware=None,
+              geometry=None,
+              ubcalc_persister=None,
+              axes_scannable_group=None,
+              energy_scannable = None,
+              energy_scannable_multiplier_to_get_KeV=1):
+    globals()['hardware'] = hardware
+    globals()['geometry'] = geometry
+    globals()['ubcalc_persister'] = ubcalc_persister
+    globals()['axes_scannable_group'] = axes_scannable_group
+    globals()['energy_scannable'] = energy_scannable
+    globals()['energy_scannable_multiplier_to_get_KeV'] = energy_scannable_multiplier_to_get_KeV
 
-UBCALC_STRATEGIES = {'vlieg': VliegUbCalcStrategy(),
-                     'willmott': WillmottHorizontalUbCalcStrategy(),
-                     'you': YouUbCalcStrategy()}
-
-
-ANGLES_TO_HKL_FUNCTIONS = {'vlieg': vliegAnglesToHkl,
-                           'willmott': angles_to_hkl,  #  TODO check return format
-                           'you': youAnglesToHkl}
-
-'''
-This scheme of configuring settings is based loosely to emulate the basics of that of
-the successful Python project Django:
-https://docs.djangoproject.com/en/dev/topics/settings/#using-settings-in-python-code
-'''
-class Settings(object):
+    
+def set_engine_name(engine_name):
+    '''This will be called by one of dcyou, dcvlieg or dcwillmot'''
+    global ubcalc_strategy
+    global angles_to_hkl_function
+    assert engine_name in _AVAILABLE_ENGINES
+    ubcalc_strategy = _UBCALC_STRATEGIES[engine_name]
+    angles_to_hkl_function = _ANGLES_TO_HKL_FUNCTIONS[engine_name]
     
     
-    def __init__(self):
-        self.GEOMETRY = None
-        self.HARDWARE = None
-        self.ENGINE_NAME = None
-        self.UBCALC_PERSISTER = None
-        
-    def configure(self,
-                 hardware=None,
-                 geometry=None,
-                 engine_name=None,
-                 ubcalc_perister=None):
-        
-        if engine_name not in AVAILABLE_ENGINES:
-            raise AssertionError('unkown engine: %s' % engine_name )
-        
-        self.GEOMETRY = geometry
-        self.HARDWARE = hardware
-        self.ENGINE_NAME = engine_name
-        self.UBCALC_PERSISTER = ubcalc_perister
-        
-    @property
-    def UBCALC_STRATEGY(self):
-        return UBCALC_STRATEGIES[self.ENGINE_NAME]
-    
-    @property
-    def ANGLES_TO_HKL_FUNCTION(self):
-        return ANGLES_TO_HKL_FUNCTIONS[self.ENGINE_NAME]
-    
-    
-        
-settings = Settings()
+_AVAILABLE_ENGINES = ('vlieg', 'willmott', 'you')
+
+
+_UBCALC_STRATEGIES = {'vlieg': diffcalc.hkl.vlieg.calc.VliegUbCalcStrategy(),
+                     'willmott': diffcalc.hkl.willmott.calc.WillmottHorizontalUbCalcStrategy(),
+                     'you': diffcalc.hkl.you.calc.YouUbCalcStrategy()}
+
+
+_ANGLES_TO_HKL_FUNCTIONS = {'vlieg': diffcalc.hkl.vlieg.calc.vliegAnglesToHkl,
+                           'willmott': diffcalc.hkl.willmott.calc.angles_to_hkl,  #  TODO check return format
+                           'you': diffcalc.hkl.you.calc .youAnglesToHkl}
