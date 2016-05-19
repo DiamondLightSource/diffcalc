662a5deeaf9e9e7168f5d6be21f78b25bd9d89c8
Rob Walton <rob.walton@diamond.ac.uk>
2016-05-19 20:34:44 +0100

    test for example five circle startup

---
+++ test/test_fivecircle_example.py
@@ -0,0 +1,28 @@
+'''
+Created on 8 May 2013
+
+@author: zrb13439
+'''
+
+import os
+import sys
+print sys.path
+
+diffcalc_path = os.path.split(os.path.split(__file__)[0])[0]
+STARTUP_PATH = os.path.join(diffcalc_path, 'example')
+print "prepended to path:", STARTUP_PATH
+sys.path.insert(0, STARTUP_PATH)
+
+from fivecircle import *
+
+ct.pause = False  # @UndefinedVariable
+
+class test_fivecircle_example():
+
+    def test_execfile(self):
+        pass
+        # the import above will test this
+
+    def test_demo_all_and_scan(self):
+        demo_all()  # @UndefinedVariable
+        demo_scan()  # @UndefinedVariable
