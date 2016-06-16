08479b0a1d294297228d65f049575bb56cda84ce
Anthony Hull <anthony.hull@diamond.ac.uk>
2016-06-16 10:24:14 +0100

    Modify diffcalc start script so it does not change working directory

--- diffcalc.sh
+++ diffcalc.sh
@@ -5,7 +5,6 @@ if [[ $# -eq 0 ]] ; then
 fi
 
 diffcalc_dir=$(dirname "${BASH_SOURCE}")
-working_dir=$PWD
 
 # Add .py suffix to file name if necessary
 file_name=$1
@@ -14,18 +13,12 @@ if [[ $file_name != *".py" ]]; then
 fi
 
 # Assume file is in examples and check it exists
-module_file="example/$file_name"
-if [ ! -f "$diffcalc_dir/$module_file" ]; then
+module_file="$diffcalc_dir/example/$file_name"
+if [ ! -f "$module_file" ]; then
     echo "$file_name not found"
     exit 1
 fi
 
-echo "Changing current directory to $diffcalc_dir"
-cd $diffcalc_dir
-
 command="ipython -i $module_file"
 echo "Starting diffcalc with command $command"
 $command
-
-echo "Restoring working directory $working_dir"
-cd $working_dir
