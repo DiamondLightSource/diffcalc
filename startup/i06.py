54f68ada6819e49e68d89856d5120fa29842163d
Irakli Sikharulidze <irakli.sikharulidze@diamond.ac.uk>
2018-10-26 11:41:12 +0100

    I06-297: add mu,gamma,phi constraints and set chi hardware limits at 90

--- startup/i06.py
+++ startup/i06.py
@@ -1,6 +1,11 @@
 from startup._common_imports import *  # @UnusedWildImport
 from diffcalc.gdasupport.minigda.scannable import ScannableMotionWithScannableFieldsBase  # @UnusedImport
 from diffcalc.gdasupport.minigda.scannable import DummyPD
+
+from diffcalc.hkl.you.geometry import YouGeometry, YouPosition
+from diffcalc.hkl.you.constraints import NUNAME
+from diffcalc.hardware import setrange
+from diffcalc.hkl.you.calc import SMALL
 if not GDA:    
     import startup._demo
 else:
@@ -12,17 +17,34 @@ LOCAL_MANUAL = "http://confluence.diamond.ac.uk/pages/viewpage.action?pageId=318
 # delta    dd2th
 # eta      ddth
 # chi      dummy
-# phi      dummy
+# phi      0
+
+class ThreeCircleI06(YouGeometry):
+    """For a diffractometer with theta two-theta geometry:
+          delta, eta
+    """
+    def __init__(self, beamline_axes_transform=None):
+        YouGeometry.__init__(self, 'threec', {'mu': 0, NUNAME: 0, 'phi': 0}, beamline_axes_transform)
+
+    def physical_angles_to_internal_position(self, physical_angle_tuple):
+        # mu, delta, nu, eta, chi, phi
+        delta_phys, eta_phys, chi_phys = physical_angle_tuple
+        return YouPosition(0, delta_phys, 0, eta_phys, chi_phys, 0, 'DEG')
+
+    def internal_position_to_physical_angles(self, internal_position):
+        clone_position = internal_position.clone()
+        clone_position.changeToDegrees()
+        _, delta_phys, _, eta_phys, chi_phys, _ = clone_position.totuple()
+        return delta_phys, eta_phys, chi_phys
 
 
 ### Create dummy scannables ###
 chi = DummyPD('chi')
-phi = DummyPD('phi')
 if GDA:  
-    print "!!! Starting LIVE diffcalc with delta(dd2th), eta(ddth), chi(dummy), phi(dummy) and denergy." 
-    _fourc = ScannableGroup('_fourc', (dd2th, ddth, chi, phi))
-    delta = _fourc.dd2th
-    eta = _fourc.ddth
+    print "!!! Starting LIVE diffcalc with delta(dd2th), eta(ddth), chi(dummy) and denergy." 
+    _threec = ScannableGroup('_threec', (dd2th, ddth, chi))
+    delta = _threec.dd2th
+    eta = _threec.ddth
     en=energy
     if float(en.getPosition()) == 0: # no energy value - dummy mode
         en(800)
@@ -30,7 +52,7 @@ if GDA:
 else:   
     delta = Dummy('delta')
     eta = Dummy('eta')
-    _fourc = ScannableGroup('_fourc', (delta, eta, chi, phi))
+    _threec = ScannableGroup('_threec', (delta, eta, chi))
     en = Dummy('en')
     en(800)
     
@@ -38,12 +60,14 @@ en.level = 3
  
 ### Configure and import diffcalc objects ###
 ESMTGKeV = 0.001
-settings.hardware = ScannableHardwareAdapter(_fourc, en, ESMTGKeV)
-settings.geometry = diffcalc.hkl.you.geometry.FourCircle()  # @UndefinedVariable
+settings.hardware = ScannableHardwareAdapter(_threec, en, ESMTGKeV)
+settings.geometry = ThreeCircleI06()  # @UndefinedVariable
 settings.energy_scannable = en
-settings.axes_scannable_group= _fourc
+settings.axes_scannable_group= _threec
 settings.energy_scannable_multiplier_to_get_KeV = ESMTGKeV
- 
+
+setrange('chi', 90 - SMALL, 90 + SMALL)
+
 from diffcalc.gdasupport.you import *  # @UnusedWildImport
  
 if GDA:
@@ -52,5 +76,3 @@ if GDA:
  
 # Load the last ub calculation used
 lastub() 
-if not GDA:
-    demo = startup._demo.Demo(globals(), 'fourc')
