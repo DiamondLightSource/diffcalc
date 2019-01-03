4273b8c6e03fa7bf44de526ec6d84bf5a5af75b4
Irakli Sikharulidze <irakli.sikharulidze@diamond.ac.uk>
2019-01-03 16:32:52 +0000

    More Jama path wrangling

--- install-jython-environment.sh
+++ install-jython-environment.sh
@@ -4,13 +4,10 @@ JYTHON_URL='http://search.maven.org/remotecontent?filepath=org/python/jython-ins
 JAMA_JAR_URL='http://search.maven.org/remotecontent?filepath=gov/nist/math/jama/1.0.3/jama-1.0.3.jar'
 
 # Install Jama jar
-mkdir -p lib
-cd lib
 wget $JAMA_JAR_URL -O jama-1.0.3.jar
-export CLASSPATH=$HOME/lib/jama-1.0.3.jar:$CLASSPATH
+export CLASSPATH=$PWD/jama-1.0.3.jar:$CLASSPATH
 
 # Install Jython
-cd $HOME
 wget $JYTHON_URL -O jython_installer.jar
 java -jar jython_installer.jar -s -d $HOME/jython
 
