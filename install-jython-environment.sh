aae1fd99c7a56e7b94a8c4a3a0dfc00f30c49ff6
Irakli Sikharulidze <irakli.sikharulidze@diamond.ac.uk>
2019-02-05 12:05:16 +0000

    SCI-7344: update build dependencies

--- install-jython-environment.sh
+++ install-jython-environment.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 
-JYTHON_URL='http://search.maven.org/remotecontent?filepath=org/python/jython-installer/2.7.1/jython-installer-2.7.1.jar'
-JAMA_JAR_URL='http://search.maven.org/remotecontent?filepath=gov/nist/math/jama/1.0.3/jama-1.0.3.jar'
+JYTHON_URL='http://central.maven.org/maven2/org/python/jython-installer/2.7.1/jython-installer-2.7.1.jar'
+JAMA_JAR_URL='http://central.maven.org/maven2/gov/nist/math/jama/1.0.3/jama-1.0.3.jar'
+COMMONS_MATH_URL='http://central.maven.org/maven2/org/apache/commons/commons-math3/3.6.1/commons-math3-3.6.1.jar'
 
-# Install Jama jar
-wget $JAMA_JAR_URL -O jama-1.0.3.jar
-export CLASSPATH=$PWD/jama-1.0.3.jar:$CLASSPATH
+# Install jars
+wget $JAMA_JAR_URL
+wget $COMMONS_MATH_URL
+export CLASSPATH=$PWD/jama-1.0.3.jar:$PWD/commons-math3-3.6.1.jar:$CLASSPATH
 
 # Install Jython
 wget $JYTHON_URL -O jython_installer.jar
