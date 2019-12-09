9b09aaba01223cd29a74fbb34681fa9e844cdb61
Irakli Sikharulidze <irakli.sikharulidze@diamond.ac.uk>
2019-12-09 13:09:00 +0000

    SCI-8390: tests for fixed detector, eta and chi mode

--- test/diffcalc/hkl/you/test_calc.py
+++ test/diffcalc/hkl/you/test_calc.py
@@ -730,6 +730,78 @@ class TestCubic_FixedDeltaEtaPhi30Mode(_TestCubic):
         for case_tuple in self.case_generator():
             yield case_tuple
 
+class TestCubic_FixedDeltaEtaChi0Mode(_TestCubic):
+
+    def setup_method(self):
+        _TestCubic.setup_method(self)
+        self.constraints._constrained = {'eta': 0, 'delta': 0, 'chi': 0}
+        #self.mock_hardware.set_lower_limit('chi', -180.)
+
+    def makes_cases(self, yrot, zrot):
+        self.zrot = zrot
+        self.yrot = yrot
+        self.wavelength = 1
+        self.cases = (
+             Pair('100', (1, 0, 0),
+                  Pos(mu=120, delta=0, nu=60, eta=0, chi=0,
+                      phi=-90 + self.zrot, unit='DEG')),
+             Pair('100-->001', (cos(4 * TORAD), 0, sin(4 * TORAD)),
+                  Pos(mu=120 - 4, delta=0, nu=60, eta=0, chi= 0,
+                      phi=-90 + self.zrot, unit='DEG')),
+             Pair('010', (0, 1, 0),
+                  Pos(mu=120, delta=0, nu=60, eta=0, chi=0,
+                      phi=self.zrot, unit='DEG')),
+             Pair('001', (0, 0, 1),
+                  Pos(mu=30, delta=0, nu=60, eta=0, chi=0,
+                      phi=0, unit='DEG'), fails=True),  # degenerate case phi||q
+             Pair('001-->100', (cos(86 * TORAD), 0, sin(86 * TORAD)),
+                  Pos(mu=30 - 4 , delta=0, nu=60, eta=0, chi=0,
+                      phi=90 + self.zrot, unit='DEG')),
+            )
+        self.case_dict = {}
+        for case in self.cases:
+            self.case_dict[case.name] = case
+
+    def test_pairs_zrot0_yrot0(self):
+        self.makes_cases(0, 0)
+        for case_tuple in self.case_generator():
+            yield case_tuple
+
+    def test_pairs_various_zrot_and_yrot0(self):
+        for zrot in [0, 2, -2,]:
+            self.makes_cases(0, zrot)
+            for case_tuple in self.case_generator():
+                yield case_tuple
+    
+class TestCubic_FixedDeltaEtaChi30Mode(_TestCubic):
+
+    def setup_method(self):
+        _TestCubic.setup_method(self)
+        self.constraints._constrained = {'eta': 0, 'delta': 0, 'chi': 30 * TORAD}
+
+    def makes_cases(self, yrot, zrot):
+        self.zrot = zrot
+        self.yrot = yrot
+        self.wavelength = 1
+        self.cases = (
+             Pair('100', (1, 0, 0),
+                  Pos(mu=120, delta=0, nu=60, eta=0, chi=30,
+                      phi=-90, unit='DEG')),
+             Pair('010', (0, 1, 0),
+                  Pos(mu=120, delta=0, nu=60, eta=0, chi=30,
+                      phi=0, unit='DEG')),
+             Pair('100-->001', (-sin(30 * TORAD), 0, cos(30 * TORAD)),
+                  Pos(mu=30, delta=0, nu=60, eta=0, chi=30,
+                      phi=0, unit='DEG')),
+            )
+        self.case_dict = {}
+        for case in self.cases:
+            self.case_dict[case.name] = case
+
+    def test_pairs_zrot0_yrot0(self):
+        self.makes_cases(0, 0)
+        for case_tuple in self.case_generator():
+            yield case_tuple
 
 class TestAgainstSpecSixcB16_270608(_BaseTest):
     '''NOTE: copied from test.diffcalc.scenarios.session3'''
